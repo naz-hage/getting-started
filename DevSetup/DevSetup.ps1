2923ed2b167134671398dc9632eaac116bf86a8e
ahmdhjj <77078034+ahmdhjj@users.noreply.github.com>
2024-06-16 18:26:41 -0400

    Add DevSetup folder and instructions (#12)
    * Add DevSetup folder and instructions
    * Fix file path in README
    * Add more tools to apps.json
    * Improve error messages in DevSetup.ps1

---
+++ DevSetup/DevSetup.ps1
@@ -0,0 +1,34 @@
+# DevSetup.ps1
+# Get the common Install module and import it
+$url = "https://raw.githubusercontent.com/naz-hage/ntools/main/DevSetup/install.psm1"
+$output = "./install.psm1"
+Invoke-WebRequest -Uri $url -OutFile $output
+Import-Module ./install.psm1 -Force
+
+$fileName = Split-Path -Leaf $PSCommandPath
+Write-OutputMessage $fileName "Started installation script."
+
+# Check if the script is running with admin rights
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-OutputMessage $fileName "Error: Please run this script as an administrator."
+    exit 1
+} else {
+    Write-OutputMessage $fileName "Admin rights detected"
+}
+
+# Install Ntools
+MainInstallApp -command install -json .\app-Ntools.json
+if ($LASTEXITCODE -ne 0) {
+    Write-OutputMessage $fileName "Error: Installation of ntools (app-Ntools.json) failed. Exiting script."
+    exit 1
+}
+
+# Install development tools for the project
+& $global:NbExePath -c install -json .\apps.json
+if ($LASTEXITCODE -ne 0) {
+    Write-OutputMessage $fileName "Error: Installation of other tools (apps.json)  failed. Exiting script."
+    exit 1
+}
+
+Write-OutputMessage $fileName "Completed installation script."
+Write-OutputMessage $fileName "EmtpyLine"
